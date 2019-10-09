2dc356342fd2880b580f38704c8bcd7418bc2c5a
Hendrik Sollich <hendrik@hoodie.de>
2019-10-09 21:58:20 +0200

    update typescript
    not happy with the `strictBindCallApply` caveats though
    see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-2.html#strictbindcallapply

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const comment = (...x: any[]) => console.log.apply(console, [`//`].concat(x));
+export const comment = (...x: any[]) => console.log.apply(console, [`//`].concat(x) as any);
 export const dir = (x: any) => console.dir(x, { colors: true, depth: 4 });
 export const odir = (x: any) => dir(JSON.parse(JSON.stringify(x, (k, v) => { if (k !== 'parent') { return v; } })));
 export const capitalize = (s: string): string => s[0].toUpperCase() + s.slice(1);
